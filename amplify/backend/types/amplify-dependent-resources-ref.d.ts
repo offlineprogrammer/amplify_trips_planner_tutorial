78c71615cc3d7b35b4226092b8fbbcf9c2016a31
offlineProgrammer <mmalaka@gmail.com>
2023-03-03 09:38:47 -0800

    Adding Storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -15,5 +15,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s37d9e620f": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
